84332e9e75314f0c24d9371cb48db5b02f02b596
Marco Frigerio <marco.frigerio@kuleuven.be>
2018-08-23 23:01:05 +0200

    Some progress in the urdf converter tests script

--- tests/urdf-conversion/runtest.sh
+++ tests/urdf-conversion/runtest.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
-if [ $# -eq 0 ]
-  then
-    echo "Error. No arguments supplied"
-    exit
-fi
+#if [ $# -eq 0 ]
+#  then
+#    echo "Error. No arguments supplied"
+#    exit
+#fi
 
-STARTDIR=`pwd`
+dir_gen="gen"
 
+mkdir -p $dir_gen
 
- ROB=$1
-URDF="`basename $ROB`.urdf"
+ in_kindsl="input/ur5.kindsl"
+  out_urdf="$dir_gen/ur5.kindsl.urdf"
+   in_urdf="input/ur5-gmv.urdf"
+out_kindsl="$dir_gen/ur5-gmv.urdf.kindsl"
 
-ilk-generator.sh --robot-model $ROB --gen-urdf $URDF
+# Convert from KinDSL to URDF and back
 
-../../urdf2kindsl/urdf2kindsl.py $URDF -o "$URDF.kindsl"
+ilk-generator.sh --robot-model $in_kindsl --gen-urdf "$out_urdf"
+../../urdf2kindsl/urdf2kindsl.py "$out_urdf" -o "$out_urdf.kindsl"
 
+# Convert from URDF to KinDSL and back
 
+../../urdf2kindsl/urdf2kindsl.py $in_urdf -o "$out_kindsl"
+ilk-generator.sh --robot-model "$out_kindsl" --gen-urdf "$out_kindsl.urdf"
