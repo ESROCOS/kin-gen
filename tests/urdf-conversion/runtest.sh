406f2a908bc31ebc4ea6846678f42be96791e14c
Marco Frigerio <marco.frigerio@kuleuven.be>
2018-08-24 11:43:02 +0200

    Finalize (??) urdf conversion tests

--- tests/urdf-conversion/runtest.sh
+++ tests/urdf-conversion/runtest.sh
@@ -6,21 +6,40 @@
 #    exit
 #fi
 
-dir_gen="gen"
+dir_gen="output"
 
 mkdir -p $dir_gen
 
- in_kindsl="input/ur5.kindsl"
-  out_urdf="$dir_gen/ur5.kindsl.urdf"
+ in_kindsl="input/ur5-kul.kindsl"
+  out_urdf="$dir_gen/ur5-kul.kindsl.urdf"
    in_urdf="input/ur5-gmv.urdf"
 out_kindsl="$dir_gen/ur5-gmv.urdf.kindsl"
 
-# Convert from KinDSL to URDF and back
+urdf2kindsl="../../urdf2kindsl/urdf2kindsl.py"
 
+# Conversions
+
+echo "Converting $in_kindsl to URDF and back ..."
 ilk-generator.sh --robot-model $in_kindsl --gen-urdf "$out_urdf"
-../../urdf2kindsl/urdf2kindsl.py "$out_urdf" -o "$out_urdf.kindsl"
+$urdf2kindsl "$out_urdf" -o "$out_urdf.kindsl"
 
-# Convert from URDF to KinDSL and back
-
-../../urdf2kindsl/urdf2kindsl.py $in_urdf -o "$out_kindsl"
+echo "Converting $in_urdf to KinDSL and back ..."
+$urdf2kindsl $in_urdf -o "$out_kindsl"
 ilk-generator.sh --robot-model "$out_kindsl" --gen-urdf "$out_kindsl.urdf"
+
+
+# Some checks
+
+echo "Generating diff between $in_kindsl and $out_urdf.kindsl ..."
+cp ../utils/diff-style.css $dir_gen
+../utils/diff2html.py --style-sheet diff-style.css $in_kindsl "$out_urdf.kindsl" > $dir_gen/kindsl_diff.html
+
+echo "Checking kinematics of $in_urdf and $out_kindsl.urdf ..."
+pos1=`$urdf2kindsl --link-origin wrist_3_link $in_urdf`
+pos2=`$urdf2kindsl --link-origin wrist_3_link $out_kindsl.urdf`
+echo "Position of wrist_3_link in model $in_urdf : $pos1" >> $dir_gen/urdf_pos
+echo "Position of wrist_3_link in model $out_kindsl.urdf : $pos2" >> $dir_gen/urdf_pos
+
+echo ""
+echo "... done. Please check the results in the $dir_gen folder"
+
