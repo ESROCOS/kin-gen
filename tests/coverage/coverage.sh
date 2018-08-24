5ff59aa01752a277797b78d1a97aa9fcdb75dfa5
Pawel <invi@vp.pl>
2018-08-24 14:20:05 +0200

    Coverage tests generation

---
+++ tests/coverage/coverage.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+rm -rf generated
+
+rm -rf compiled
+
+COVERAGE_ROOT=$KIN_GEN_ROOT/tests/coverage
+
+cd $KIN_GEN_ROOT
+./ilk-generator.sh --robot $COVERAGE_ROOT/sample_"$1"/model/ur5.kindsl --query $COVERAGE_ROOT/sample_"$1"/model/ur5.dtdsl --output-dir $COVERAGE_ROOT/generated
+
+cd $KIN_GEN_ROOT/ilk-compiler
+./ilk-compiler.lua -b eigen --indir $COVERAGE_ROOT/generated --outdir $COVERAGE_ROOT/compiled
+
+cd $COVERAGE_ROOT/compiled
+make ur5_"$1"_timing_dbg
+
+./ur5_"$1"_timing_dbg 1 1
+
+gcov --relative-only ur5_"$1"_timing.cpp
+lcov --no-external -t "blah" -o ur5_"$1"_timing.info -c -d `pwd` -d `pwd`
+genhtml -o ../coverage_html_"$1"/ ur5_"$1"_timing.info
