8c815add58834caae970c6ba95c1da04b7e72a56
Marco Frigerio <marco.frigerio@kuleuven.be>
2020-01-21 09:45:04 +0100

    Set and use a local PYTHONPATH

--- ilk-generator.sh
+++ ilk-generator.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-export PYTHONPATH="$PYTHONPATH:$KIN_GEN_ROOT/robot-model-tools:$KIN_GEN_ROOT/ilk-generator"
-
+PYTHONPATH="$KIN_GEN_ROOT/robot-model-tools:$KIN_GEN_ROOT/ilk-generator" \
 $KIN_GEN_ROOT/ilk-generator/ilkgen.py $@
 
