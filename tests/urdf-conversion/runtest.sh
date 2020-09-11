b70af01e5988deddd1e0e4bf966e6093cdc3b2b1
Marco Frigerio <marco.frigerio@kuleuven.be>
2020-09-11 17:03:19 +0200

    Update the urdf2kindsl submodule and fix the tests
    The kindsl-to-urdf is now performed by the `rmt` tool
    (and not by the ilk-generator, which does not have that
    functionality anymore).
    
    Force the old diff2html.py to be interpreted with python2,
    because it does not work with 3

--- tests/urdf-conversion/runtest.sh
+++ tests/urdf-conversion/runtest.sh
@@ -16,16 +16,17 @@ mkdir -p $dir_gen
 out_kindsl="$dir_gen/ur5-gmv.urdf.kindsl"
 
 urdf2kindsl="../../urdf2kindsl/urdf2kindsl.py"
+kindsl2urdf="rmtool.sh exp -f urdf"
 
 # Conversions
 
 echo "Converting $in_kindsl to URDF and back ..."
-ilk-generator.sh --robot-model $in_kindsl --gen-urdf "$out_urdf"
-$urdf2kindsl "$out_urdf" -o "$out_urdf.kindsl"
+$kindsl2urdf -o  $out_urdf          $in_kindsl
+$urdf2kindsl -o "$out_urdf.kindsl"  $out_urdf
 
 echo "Converting $in_urdf to KinDSL and back ..."
-$urdf2kindsl $in_urdf -o "$out_kindsl"
-ilk-generator.sh --robot-model "$out_kindsl" --gen-urdf "$out_kindsl.urdf"
+$urdf2kindsl -o "$out_kindsl"      $in_urdf
+$kindsl2urdf -o "$out_kindsl.urdf" $out_kindsl
 
 
 # Some checks
@@ -35,8 +36,8 @@ cp ../utils/diff-style.css $dir_gen
 ../utils/diff2html.py --style-sheet diff-style.css $in_kindsl "$out_urdf.kindsl" > $dir_gen/kindsl_diff.html
 
 echo "Checking kinematics of $in_urdf and $out_kindsl.urdf ..."
-pos1=`$urdf2kindsl --link-origin wrist_3_link $in_urdf`
-pos2=`$urdf2kindsl --link-origin wrist_3_link $out_kindsl.urdf`
+pos1=`$urdf2kindsl --link-origin wrist_3 $in_urdf`
+pos2=`$urdf2kindsl --link-origin wrist_3 $out_kindsl.urdf`
 echo "Position of wrist_3_link in model $in_urdf : $pos1" > $dir_gen/urdf_pos
 echo "Position of wrist_3_link in model $out_kindsl.urdf : $pos2" >> $dir_gen/urdf_pos
 
